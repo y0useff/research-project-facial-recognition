9b53cdd6a3f9cdc662208a05fc60d6c77b17a626
srivallisreya <52923748+srivallisreya@users.noreply.github.com>
2023-11-17 16:49:09 -0500

    Add files via upload

---
+++ robot sound.py
@@ -0,0 +1,28 @@
+from gtts import gTTS
+import pygame
+import time
+
+#lets say input from detect function can be a name of the person detected.
+def robot_sound(detect_name):
+    text = "Found {}"
+    m=text.format(detect_name)
+    tts = gTTS(text=m, lang='en',slow=True)
+    tts.save("output.mp3")
+    play_robot_voice("output.mp3")
+    time.sleep(2)
+    play_robot_voice("output.mp3")
+    time.sleep(2)
+    play_robot_voice("output.mp3")
+
+
+
+def play_robot_voice(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+robot_sound("name")
+
+
+
+
+
