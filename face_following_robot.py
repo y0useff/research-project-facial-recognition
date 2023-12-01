e577f932aee4bdc57a7a5d782612005ddfa56a87
agadomi <125948866+agadomi@users.noreply.github.com>
2023-12-01 15:27:48 -0500

    Update face_following_robot.py

--- face_following_robot.py
+++ face_following_robot.py
@@ -14,12 +14,9 @@ GPIO.setup(right_motor_pin, GPIO.OUT)
 # Load the pre-trained face detection classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
-
-# Set the camera resolution
-cap.set(3, 640)  # Width
-cap.set(4, 480)  # Height
+# Load the provided image
+provided_image_path = 'path/to/provided/image.jpg'  # Replace with the actual path to the provided image
+provided_image = cv2.imread(provided_image_path)
 
 # Function to control the robot based on face position
 def control_robot(face_x, frame_width):
@@ -39,44 +36,31 @@ def control_robot(face_x, frame_width):
         GPIO.output(left_motor_pin, GPIO.HIGH)
         GPIO.output(right_motor_pin, GPIO.HIGH)
 
-# Main loop
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if not ret:
-        break
+# Perform face detection on the provided image
+gray_provided_image = cv2.cvtColor(provided_image, cv2.COLOR_BGR2GRAY)
+faces_in_provided_image = face_cascade.detectMultiScale(
+    gray_provided_image,
+    scaleFactor=1.3,
+    minNeighbors=5,
+    minSize=(30, 30),
+    maxSize=(600, 600)
+)
 
-    # Convert the frame to grayscale for face detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Process each detected face in the provided image
+for (x, y, w, h) in faces_in_provided_image:
+    # Draw rectangle around the face
+    cv2.rectangle(provided_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    # Detect faces in the image
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
-        minNeighbors=5,
-        minSize=(30, 30),  # Adjust this based on the desired minimum face size
-        maxSize=(600, 600)  # Adjust this based on the desired maximum face size
-    )
+    # Control the robot based on the face position
+    control_robot(x + w // 2, provided_image.shape[1])
 
-    # Process each detected face
-    for (x, y, w, h) in faces:
-        # Draw rectangle around the face
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # Play sound when a person is detected
+    play_sound()
 
-        # Control the robot based on the face position
-        control_robot(x + w // 2, frame.shape[1])
-
-        # Play sound when a person is detected
-        play_sound()
-
-    # Display the resulting frame
-    cv2.imshow('Face Following Robot', frame)
-
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release resources
-cap.release()
+# Display the resulting image with detected faces
+cv2.imshow('Detected Faces in Provided Image', provided_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Release GPIO resources
 GPIO.cleanup()
